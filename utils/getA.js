83fd434367a1553eae94f70add8a97f7f044e813
Soumyadip Mondal <mandal.soumyadip9714@gmail.com>
2021-02-24 20:30:04 +0530

    [FIX] Fauna Client object out of scope

--- utils/getA.js
+++ utils/getA.js
@@ -2,12 +2,12 @@
 const faunadb = require('faunadb')
 const q = faunadb.query
 
-exports.getA = () => {
-    console.log('Function `getQ` invoked');
-
-    const client = new faunadb.Client({
+const client = new faunadb.Client({
     secret: process.env.FAUNADB_SERVER_SECRET
-    });
+});
+
+exports.getA = () => {
+    console.log('Function `getA` invoked');
 
     return client.query(q.Paginate(
         q.Match(
